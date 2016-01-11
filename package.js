b1593930bb9c8204077302b7437086e6521e0551
Eric Le Lay <eric@eliga.fr>
2016-01-11 12:30:44 +0100

    update to latest dburles code + increase inactive threshold to 45s

--- package.js
+++ package.js
@@ -1,12 +1,20 @@
 Package.describe({
-  summary: "A package to help track users' presence"
+  name: "tmeasday:presence",
+  summary: "A package to help track users' presence (fork from https://github.com/NikolayH86/meteor-presence)",
+  version: "1.0.6",
+  git: "https://github.com/welelay/meteor-presence.git"
 });
 
-Package.on_use(function (api) {
-  api.add_files('lib/common.js', ['client', 'server']);
-  api.add_files('lib/client.js', 'client');
-  api.add_files('lib/server.js', 'server');
+Package.onUse(function (api) {
+  api.versionsFrom('1.2');
+  api.use('tracker', 'client');
+  api.use('mongo');
+  api.use('underscore');
 
-  api.export('Presences', ['client', 'server']);
-  api.export('Presence', ['client', 'server']);
+  api.addFiles('lib/common.js');
+  api.addFiles('lib/client.js', 'client');
+  api.addFiles('lib/server.js', 'server');
+
+  api.export('Presences');
+  api.export('Presence');
 });
